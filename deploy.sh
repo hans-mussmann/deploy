67f6dbba7921cff332f92d1b6467820526de9e78
Hans Mussmann <hans-mussmann@users.noreply.github.com>
2015-01-21 08:29:23 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 
-###################################################
-# Bash script to deploy an application from a repo
-###################################################
+## Author: Hans Mussmann
 
-# Make deploy Path on Server
+echo -e "\n-----------------------------------------------------\n"
+echo -e ' - Deploy                                    [START]'
 
-# Extract Repo to Deploy Path (SVN GIT etc)
+_BRANCH='master'
+_REPO='reapster.com.au'
+_SITE='reapster.com.au'
 
-# Restart Servers (HTTPD MYSQL etc)
+echo -e " - Using repo $_REPO"
+cd /var/repo/$_REPO/ || exit 1
+
+echo -e " - Fetching branch $_BRANCH"
+git branch -r
+git checkout $_BRANCH
+git pull
+
+echo -e ' - Archive static files e.g. Images and Uploads'
+cp -Ru /var/www/$_SITE/images/. /var/www/archive/$_SITE/images/
+cp -Ru /var/www/$_SITE/uploads/. /var/www/archive/$_SITE/uploads/
+
+echo -e " - Deploy site $_SITE"
+git archive $_BRANCH | tar -x -C /var/www/$_SITE/
+
+echo -e ' - Cleaning up e.g. git'
+rm -rf /var/www/$_SITE/.git*
+
+echo -e ' - Restore archived files'
+cp -R /var/www/archive/$_SITE/images/*.jp* /var/www/$_SITE/images/
+cp -R /var/www/archive/$_SITE/uploads/. /var/www/$_SITE/uploads/
+
+cd ~/
+
+echo -e ' - Deploy                                      [DONE]'
+echo -e "\n-----------------------------------------------------\n"
 
